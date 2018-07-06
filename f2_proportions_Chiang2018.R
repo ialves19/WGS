c35b39da43a02b0dd3743dbe66cc4a594a3431ad
ialves19 <isa.mta@gmail.com>
2018-07-06 10:31:31 +0200

    proper computation of allele sharing (dij/M)/2xixj

--- f2_proportions_Chiang2018.R
+++ f2_proportions_Chiang2018.R
@@ -14,28 +14,22 @@ acronyms <- c("BRE", "PL", "C", "N", "LPCH", "NCP", "AL")
 sampleSizes <- c(323, 297, 29, 19, sum(24, 22), sum(76,11), sum(15,27))
 totalSize <- 856
 
-#8 regions
-# allPops <- c("BRETAGNE", "PAYS-DE-LA-LOIRE", "CENTRE", "POITOU-CHARENTES", "LIMOUSIN", "NORD-PAS-DE-CALAIS", "LORRAINE", "ALSACE")
-# acronyms <- c("BRET", "PDDL", "CEN", "PC", "LIM", "NPDC", "LOR", "ALS")
-
-#16 populations
-# allPops <- c("COTES-DARMOR", "FINISTERE", "ILLE-ET-VILAINE", "MORBIHAN", "LOIRE-ATLANTIQUE", "MAINE-ET-LOIRE", "MAYENNE", "SARTHE", "VENDEE", 
-#              "NORD", "PAS-DE-CALAIS",  "MEURTHE-ET-MOSELLE.MOSELLE.VOSGES", "BAS-RHIN.HAUT-RHIN", "CORREZE.CREUSE.HAUTE-VIENNE",  
-#              "VIENNE.DEUX-SEVRES.CHARENTE", "LOIR-ET-CHER.INDRE-ET-LOIRE.LOIRET")
-# acronyms <- c("CD_B", "F_B", "IEV_B", "M_B", "LA_PL", "ML_PL", "MNE_PL", "S_PL", "V_PL","N_NPC", "PC_NPC", "MMV_L", "BRHR_A",
-#               "CCHV_L", "VDSC_PC", "LIL_C")
+# allPops <- c("FINISTERE", "COTES-DARMOR", "MORBIHAN", "ILLE-ET-VILAINE", "LOIRE-ATLANTIQUE", "MAYENNE", "MAINE-ET-LOIRE", "SARTHE", "VENDEE")
+# acronyms <- c("FIN", "CDA", "MOR", "IEV", "LA", "MAY", "MEL", "SAR", "VEN")
+# sampleSizes <- c(109, 70, 80, 64, 88, 43, 72, 23, 71)
+# totalSize <- 856
 
 chrms <- paste0("chr", 1:22)
-par(mfrow=c(length(allPops)+1,1), mar=c(1,4,1,1))
+
 #colors_reg <- randomColor(length(allPops), luminosity = "dark")
-#colors_reg <- c("#01665e", "#35978f", "#80cdc1", "#c7eae5",  "#f6e8c3", "#dfc27d", "#bf812d","#8c510a")
-#colors_reg <- c("#b2182b", "#d6604d", "#f4a582", "#fddbc7",  "#d1e5f0", "#92c5de", "#4393c3","#2166ac")
+#colors_reg <- c("#b2182b", "#d6604d", "#f4a582", "#fddbc7",  "#f7f7f7",  "#d1e5f0", "#92c5de", "#4393c3","#2166ac")
 colors_reg <- c("#01665e", "#5ab4ac", "#c7eae5", "#f5f5f5", "#f6e8c3", "#d8b365", "#8c510a")
 #chrID <- "chr22"
 count_p <- 1
-cat(paste0(paste0(allPops, collapse = "\t"), "\n"), file = paste0(folderName, "/f2_mac2Global.txt"), sep = "\t")
+cat(paste0(paste0(allPops, collapse = "\t"), "\n"), file = paste0(folderName, "/f2_mac2Global_ALLreg.txt"), sep = "\t")
 
 pPop_total_nb_double_global <- list()
+doubleSharing_acrossPop <- list()
 
 for (popToAnalise in allPops) {
   
@@ -45,18 +39,18 @@ for (popToAnalise in allPops) {
   for (chr in chrms) {
     
     list_p_chr[[chr]] <- read.table(paste0(folderName, "/", prefix, ".", chr, ".", sufix, ".", popToAnalise, ".frq.count"), header = F, skip = 1)
-    total_nb_double_global [[chr]] <- nrow(list_p_chr[[chr]])
+    total_nb_double_global[[chr]] <- nrow(list_p_chr[[chr]])
   }
   tb_open <- do.call(rbind,list_p_chr)
-  pPop_total_nb_double_global[[popToAnalise]] <- sum(unlist(total_nb_double_global))
+  pPop_total_nb_double_global[[popToAnalise]] <- sum(unlist(total_nb_double_global)) #this value should not differ across pops
   #count doubletons
-  nb_double_withinPop <- nrow(tb_open[which(tb_open[,6] == 2),])*2
+  nb_double_withinPop <- nrow(tb_open[which(tb_open[,6] == 2),])
   
   nb_shared_double <- nrow(tb_open[which(tb_open[,6] == 1),])
   double_shared <- paste0(tb_open[which(tb_open[,6] == 1),1], "_",tb_open[which(tb_open[,6] == 1),2])
   
   dd_shared_pop_and_other <- list()
-
+  relative_sample_sizes <- list()
   
   for (p in allPops[which(allPops != popToAnalise)]) {
     
@@ -64,38 +58,114 @@ for (popToAnalise in allPops) {
     for (chr in chrms) {
       list_op_chr[[chr]] <- read.table(paste0(folderName, "/", prefix, ".", chr, ".", sufix, ".", p, ".frq.count"), header = F, skip = 1)
     }
-      #p <- "COTES-DARMOR"
+    #p <- "COTES-DARMOR"
     tb_other_pop <- do.call(rbind,list_op_chr)
     
     shared_dd_other <- nrow(tb_other_pop[which(tb_other_pop[,6] == 1),])
     dd_pos_shared_other <- paste0(tb_other_pop[which(tb_other_pop[,6] == 1),1], "_",tb_other_pop[which(tb_other_pop[,6] == 1),2])
     dd_shared_pop_and_other[[p]] <- length(intersect(double_shared, dd_pos_shared_other))
     
+    relative_sample_sizes[[p]] <- 2*(sampleSizes[which(allPops == popToAnalise)]/totalSize)*(sampleSizes[which(allPops == p)]/totalSize)
+    
   }
   
   v_final <- rep(0,length(allPops))
   rel_size <- rep(0,length(allPops))
   names(v_final) <- allPops
-  v_final[match(names(unlist(dd_shared_pop_and_other)),allPops)] <- as.numeric(unlist(dd_shared_pop_and_other)/(totalSize*2))
-  v_final[which(allPops == popToAnalise)] <- nb_double_withinPop/(totalSize*2)
-  rel_size[match(names(unlist(dd_shared_pop_and_other)),allPops)] <- sampleSizes[match(names(unlist(dd_shared_pop_and_other)),allPops)]/totalSize
-  rel_size[which(allPops == popToAnalise)] <- sampleSizes[which(allPops == popToAnalise)]/totalSize
+  v_final[match(names(unlist(dd_shared_pop_and_other)),allPops)] <- as.numeric(unlist(dd_shared_pop_and_other)/pPop_total_nb_double_global[[1]])
+  v_final[which(allPops == popToAnalise)] <- nb_double_withinPop/pPop_total_nb_double_global[[1]]
   
+  rel_size[match(names(unlist(dd_shared_pop_and_other)),allPops)] <- unlist(relative_sample_sizes)
+  rel_size[which(allPops == popToAnalise)] <- (sampleSizes[which(allPops == popToAnalise)]/totalSize)^2
+  
+  doubleSharing_acrossPop[[popToAnalise]] <- v_final/rel_size
   barplot(v_final/rel_size, names.arg = "", col = colors_reg, axes = F)
   mtext(side=2, text=acronyms[count_p], las=2, line=0, cex = 1) 
   rect((which(allPops == popToAnalise)-1)+0.2*(which(allPops == popToAnalise)), 0, which(allPops == popToAnalise)+0.2*(which(allPops == popToAnalise)), as.numeric(v_final[which(allPops == popToAnalise)]/rel_size[which(allPops == popToAnalise)]), lwd=3)
-  cat(paste0(paste0(c(nb_double_withinPop, as.numeric(unlist(dd_shared_pop_and_other))), collapse = "\t"), "\n"), file = paste0(folderName, "/f2_mac2Global.txt"), sep = "\t", append = T)
+  cat(paste0(paste0(c(nb_double_withinPop, as.numeric(unlist(dd_shared_pop_and_other))), collapse = "\t"), "\n"), file = paste0(folderName, "/f2_mac2Global_BRE_PDLL.txt"), sep = "\t", append = T)
   count_p <- count_p+1
 }
 
+m_allele_sharing <- matrix(unlist(doubleSharing_acrossPop), ncol = length(doubleSharing_acrossPop), byrow = T)
+colnames(m_allele_sharing) <- acronyms
+rownames(m_allele_sharing) <- acronyms
+
+##########################
+## generating PDF       ##
+##########################
+source("https://bioconductor.org/biocLite.R")
+# biocLite()
+# biocLite("made4")
+# install.packages("corrplot")
+library(made4)
+library(gtools)
+library(corrplot)
+
+pdf(file = paste0(folderName, "/f2_globalMac2_ALLreg.pdf"), height=8, width = 8)
+par(mfrow=c(length(allPops)+1,1), mar=c(1,4,1,1))
+for (c in 1:nrow(m_allele_sharing)) {
+  
+  barplot(m_allele_sharing[c,], names.arg = "", col = colors_reg, axes = F, ylim=c(0, max(m_allele_sharing)))
+  mtext(side=2, text=acronyms[c], las=2, line=0, cex = 1) 
+  rect((which(allPops == allPops[c])-1)+0.2*(which(allPops == allPops[c])), 0, which(allPops == allPops[c])+0.2*(which(allPops == allPops[c])), 
+       as.numeric(m_allele_sharing[c,which(allPops == allPops[c])]), lwd=3)
+  
+}
+abline(h=0, col = "black")
 v_x <- rep(0, length(allPops))
 names(v_x) <- acronyms
 par(mar=c(0,3,0,1))
 plot(v_x, type = "n", xlab = "", ylab = "", xlim = c(0,19.7), ylim = c(0,10), axes = F)
 #axis(1, at=seq(0.7,19.2, by = 1.2), labels = FALSE, las=2, line=2, outer=F, tick = F, col = NULL, col.ticks = NULL)
-text(x=seq(1.5,18.5, length.out = 7), y=10, labels = acronyms, srt=45, pos = 1, cex = 1.2, offset = 1)
+text(x=seq(1,18.5, length.out = length(acronyms)), y=10, labels = acronyms, srt=45, pos = 1, cex = 1.5, offset = 1)
 #par(mar=c(6,4,0,1))
+dev.off()
+
+##########################
+## generating PNG       ##
+##########################
+
+png(file =  paste0(folderName, "/f2_globalMac2_ALLreg.png"), height=600, width = 600)
+par(mfrow=c(length(allPops)+1,1), mar=c(1,4,1,1))
+for (c in 1:nrow(m_allele_sharing)) {
+  
+  barplot(m_allele_sharing[c,], names.arg = "", col = colors_reg, axes = F, ylim=c(0, max(m_allele_sharing)))
+  mtext(side=2, text=acronyms[c], las=2, line=0, cex = 1) 
+  rect((which(allPops == allPops[c])-1)+0.2*(which(allPops == allPops[c])), 0, which(allPops == allPops[c])+0.2*(which(allPops == allPops[c])), 
+       as.numeric(m_allele_sharing[c,which(allPops == allPops[c])]), lwd=3)
+  
+}
+abline(h=0, col = "black")
+v_x <- rep(0, length(allPops))
+names(v_x) <- acronyms
+par(mar=c(0,3,0,1))
+plot(v_x, type = "n", xlab = "", ylab = "", xlim = c(0,19.7), ylim = c(0,10), axes = F)
+#axis(1, at=seq(0.7,19.2, by = 1.2), labels = FALSE, las=2, line=2, outer=F, tick = F, col = NULL, col.ticks = NULL)
+text(x=seq(1,18.5, length.out = length(acronyms)), y=10, labels = acronyms, srt=45, pos = 1, cex = 1.5, offset = 1)
+#par(mar=c(6,4,0,1))
+dev.off()
+
+#########################
+## Defining colors
+#########################
+col.to.paint <- colorRampPalette(colors_reg)
+
+png(file=paste0(folderName, "/f2_globalMac2_ALLreg_corrplot.png"), height=600, width = 600)
+corrplot(cor(t(m_allele_sharing)), method = "color",
+         col = col.to.paint(20), cl.length = 11, tl.col="black")
+
+dev.off()
 
 
+png(file=paste0(folderName,"/f2_globalMac2_ALLreg_heatplot.png"), height=700, width = 600)
+heatplot(m_allele_sharing, scale = "none", dend="both", method = "average", dualScale = T, zlim = c(-1.13,2.57), 
+         cols.default = T,lowcol = "blue", highcol = "red")
+dev.off()
+
+#########################
+## Print table
+#########################
+
+write.table(m_allele_sharing, file = paste0(folderName,"/f2_globalMac2_ALLreg_m.txt"), row.names = F, col.names = T, sep = "\t", quote = F)
 
 
--- plotting_geo_propF2.R
+++ plotting_geo_propF2.R
@@ -1,32 +1,44 @@
-folderName <- "/Users/isabelalves/Dropbox/instDuThorax/"
+#install.packages("Hmisc")
 
-dist_m <- read.table(paste0(folderName,"/samples/diff_propf2.txt"), header = T, sep = "\t")
-geo_m <- read.table(paste0(folderName, "/samples/dist_m_West.txt"), header = T, sep = "\t")
+folderName <- "/home/isabelalves/Dropbox/instDuThorax"
+
+dist_m <- read.table(paste0(folderName,"/f2/f2_globalMac2_ALLreg_m.txt"), header = T, sep = "\t")
+geo_m <- read.table(paste0(folderName, "/f2/dist_m_ALLreg.txt"), header = T, sep = "\t")
 colnames(dist_m)
 colnames(geo_m) <- colnames(dist_m)
+
+#removing Normandie
+# dist_m_tmp <- cbind(dist_m[,1:3],dist_m[,5:7])
+# geo_m_tmp <- cbind(geo_m[,1:3],geo_m[,5:7])
+# 
+# dist_m <- rbind(dist_m_tmp[1:3,],dist_m_tmp[5:7,])
+# geo_m <- rbind(geo_m_tmp[1:3,],geo_m_tmp[5:7,])
+# 
 dist_v_tmp <- c()
 geo_v_tmp <- c()
 
-colors_reg <- c("#b2182b", "#d6604d", "#f4a582", "#fddbc7",  "#d1e5f0", "#92c5de", "#4393c3","#2166ac")
-acronyms <- c("FIN", "CDA", "MOR", "IEV", "LA", "MAY", "MEL", "VEN")
-png(file=paste0(folderName, "/f2/corr_distkm_f2.png"), width = 800, height = 400)
+colors_reg <- c("#01665e", "#5ab4ac", "#c7eae5", "#f6e8c3", "#d8b365", "#8c510a")
+acronyms <- c("BRE", "PL", "C", "LPCH", "NCP", "AL")
+m_tmp <- list()
+pointSyb <- c(7,15,16,17,18,19,20,21, 12)
+png(file=paste0(folderName, "/f2/corr_distkm_f2_ALLreg.png"), width = 800, height = 400)
 par(mfrow=c(1,2), mar=c(4,4,2,2))
 for (rowNb in 1:nrow(dist_m)) {
   
   #rowNb <- 1
-  m_tmp[[rowNb]] <- rbind(dist_m[rowNb,], geo_m[rowNb,])
+  m_tmp[[rowNb]] <- rbind(abs(dist_m[rowNb,]-dist_m[rowNb,rowNb])/dist_m[rowNb,rowNb], geo_m[rowNb,])
   if (rowNb == 1) {
     
-    plot(x=as.numeric(geo_m[rowNb,]), y=as.numeric(dist_m[rowNb,]), col=colors_reg[rowNb], pch=19, 
-         xlab = "Distance km", ylab= "Difference in allele sharing f2", xlim = c(0,280), ylim=c(0,0.12))
+    plot(x=as.numeric(geo_m[rowNb,]), y=as.numeric(dist_m[rowNb,]), col=colors_reg[1], pch=19, 
+         xlab = "Distance km", ylab= "Difference in allele sharing f2", xlim = c(0,max(geo_m)), ylim=c(min(dist_m), max(dist_m)))
     dist_v_tmp <- as.numeric(dist_m[rowNb,])
     geo_v_tmp <- as.numeric(geo_m[rowNb,])
     
     
-  } else {
-    points(x=as.numeric(geo_m[rowNb,]), y=as.numeric(dist_m[rowNb,]),col=colors_reg[rowNb], pch=19)
-    dist_v_tmp <- c(dist_v_tmp,as.numeric(dist_m[rowNb,]))
-    geo_v_tmp <- c(geo_v_tmp, as.numeric(geo_m[rowNb,]))
+  } else if (rowNb < nrow(dist_m)) {
+    points(x=as.numeric(geo_m[rowNb,c(1:nrow(dist_m))[which(c(1:nrow(dist_m)) >= rowNb)]]), y=as.numeric(dist_m[rowNb,][c(1:nrow(dist_m))[which(c(1:nrow(dist_m)) >= rowNb)]]),col=colors_reg[1], pch=19)
+    dist_v_tmp <- c(dist_v_tmp,as.numeric(dist_m[rowNb,][c(1:nrow(dist_m))[which(c(1:nrow(dist_m)) >= rowNb)]]))
+    geo_v_tmp <- c(geo_v_tmp, as.numeric(geo_m[rowNb,][c(1:nrow(dist_m))[which(c(1:nrow(dist_m)) >= rowNb)]]))
     
   }
   
@@ -35,13 +47,14 @@ for (rowNb in 1:nrow(dist_m)) {
 cor(x=geo_v_tmp, y=dist_v_tmp)
 sum_lm <- summary(lm(dist_v_tmp~geo_v_tmp))
 abline(lm(dist_v_tmp~geo_v_tmp))
-legend("topleft", legend = acronyms, fill = colors_reg, bty = "n", cex = 1)
-legend("bottomright", legend = paste0("R-squared: ", round(sum_lm$adj.r.squared, digits = 3)), bty = "n")
+#legend("topright", legend = acronyms, fill = colors_reg, bty = "n", cex = 1)
+legend(x=100, y=2.0, legend = paste0("R-squared= ", round(sum_lm$adj.r.squared, digits = 3), 
+                                     "\n", "p-value= ",  format.pval(sum_lm$coefficients[2,4], digits=4, scientific = TRUE)), bty = "n")
 mean_diff <- unlist(lapply(1:length(m_tmp), function(x) {  mean(as.numeric(m_tmp[[x]][1,-x]))}))
 names(mean_diff) <- colnames(dist_m)
-barplot(sort(mean_diff, decreasing = T), col = colors_reg[order(mean_diff, decreasing = T)], ylim = c(0,0.10), xaxt="n",
+barplot(sort(mean_diff, decreasing = T), col = colors_reg[order(mean_diff, decreasing = T)], ylim = c(0,max(mean_diff)), xaxt="n",
         ylab = "Average differences in allele sharing, f2")
-text(x=seq(0.3,8.9, length.out = 8), y=0, labels = acronyms[order(mean_diff, decreasing = T)], srt=60, xpd=TRUE, pos = 1, cex = 1, offset = 1.2)
+text(x=seq(0.5,length(acronyms)+length(acronyms)*0.13, length.out = length(acronyms)), y=0, labels = acronyms[order(mean_diff, decreasing = T)], srt=60, xpd=TRUE, pos = 1, cex = 1, offset = 1.2)
 dev.off()
 length(dist_v_tmp)
 length(geo_v_tmp)
